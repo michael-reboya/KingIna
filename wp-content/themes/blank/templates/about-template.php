b4435c6b354d889bddf003278fe1e0972b90efed
michael-reboya <michaeljohnreboya@gmail.com>
2021-04-12 15:24:57 +0800

    first backup

---
+++ wp-content/themes/blank/templates/about-template.php
@@ -0,0 +1,18 @@
+<?php 
+	/* Template Name: ABOUT */
+get_header();
+ ?>
+
+<div class="container">
+	<h1>About shits</h1>
+	<h4>shits is always a shit</h4>
+	<p>AKNAWKJejawheawgkegawgeaWjgaweawgekjwaejawgekjawegkjlawjkeawkjgekjgawekgjjkl</p>
+</div>
+
+<div class="container deputa taning-sir">
+	<h1>About shits</h1>
+	<h4>shits is always a shit</h4>
+	<p>AKNAWKJejawheawgkegawgeaWjgaweawgekjwaejawgekjawegkjlawjkeawkjgekjgawekgjjkl</p>
+
+</div>
+ <?php get_footer() ?>
